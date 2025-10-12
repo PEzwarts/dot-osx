b619af0a8c1eab4bb7d8c3edb5a1ac19a75dbc58
PEzwarts <pe.zwarts@tutamail.com>
2025-10-12 23:41:48 +0000

    Update dot

--- shell/fish.sh
+++ shell/fish.sh
@@ -1,5 +1,5 @@
-if [[ "$(echo $SHELL)" == "/bin/zsh" ]]; then
-  if [[ $(cat /etc/shells | grep "/opt/homebrew/bin/fish") ]]; then
+if [ "$(echo $SHELL)" == "/bin/zsh" ]; then
+  if [ $(cat /etc/shells | grep "/opt/homebrew/bin/fish") ]; then
     echo /opt/homebrew/bin/fish | sudo tee -a /etc/shells
   fi
 
